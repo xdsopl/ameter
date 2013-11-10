c979bb45ac85b23179e9d2f183c71c976c38baf9
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2013-11-10 21:08:51 +0100

    added newline to end of copyright message

--- ameter.c
+++ ameter.c
@@ -40,7 +40,7 @@ void print_copyright()
 	fputs(	"ameter - stat system components in a simple terminal\n"
 		"Written in 2013 by <Ahmet Inan> <xdsopl@googlemail.com>\n"
 		"To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.\n"
-		"You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>."
+		"You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.\n"
 	, stderr);
 }
 
